339f913a35a33d251b0afe02a46076356a963081
Pagadalajayaram <147790014+Pagadalajayaram@users.noreply.github.com>
2026-06-08 20:45:36 +0530

    code creted for user registerrequest authservices file

---
+++ backend/src/main/java/com/campusconnect/backend/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.campusconnect.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.campusconnect.backend.dto.RegisterRequest;
+import com.campusconnect.backend.entity.User;
+import com.campusconnect.backend.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody RegisterRequest request) {
+        return authService.register(request);
+    }
+}
